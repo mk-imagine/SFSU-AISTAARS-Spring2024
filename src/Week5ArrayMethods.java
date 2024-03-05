1135c24b6f0948c1b0ea351041551ad30a7aa04c
Mark Kim <mkim797@yahoo.com>
2024-03-04 17:27:01 -0800

    array methods complete

--- src/Week5ArrayMethods.java
+++ src/Week5ArrayMethods.java
@@ -5,7 +5,7 @@ public class Week5ArrayMethods {
     Create the following methods and a set of tests in the main method to test your methods:
 
     initOddArray - this method should generate an array of arbitrary size (we can call this size n).  The array should
-    have incrementing odd numbers up to n, then decrementing odd numbers back down to 1.
+    have incrementing odd numbers up to n (but maybe not including n), then decrementing odd numbers back down to 1.
     EXAMPLE: if the array size is 10, it should return an array with the following elements: [1, 3, 5, 7, 9, 9, 7, 5, 3,
     1].  If the array size is 5, it should return an array with the following elements: [1, 3, 5, 3, 1]
 
@@ -30,4 +30,74 @@ public class Week5ArrayMethods {
     private static final String[] RANKS = { "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4"
             , "3", "2" };
     private static final int DECK_SIZE = 52;
+
+    public static void main(String[] args) {
+        int[] oAry = initOddArray(5);
+        printAry(oAry);
+    }
+
+    public static int[] initOddArray(int n) {
+        if (n == 0) {
+            return new int[0];
+        }
+
+        int[] oAry = new int[n];
+        int num = -1;
+        if (n % 2 == 0) {
+            for (int i = 0; i < n; i++) {
+                if (i < n/2) {
+                    num += 2;
+                    oAry[i] = num;
+                } else {
+                    oAry[i] = num;
+                    num -= 2;
+                }
+            }
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (i <= n/2) {
+                    num += 2;
+                    oAry[i] = num;
+                } else {
+                    num -= 2;
+                    oAry[i] = num;
+                }
+            }
+        }
+
+        return oAry;
+    }
+
+    public static void printAry(int[] ary) {
+        for (int i = 0; i < ary.length; i++) {
+            System.out.print(ary[i] + " ");;
+        };
+    }
+
+    public static void printAry(String[] ary) {
+        for (int i = 0; i < ary.length; i++) {
+            System.out.print(ary[i]);;
+        }
+    }
+
+    public static String[][] initCardDeck() {
+        String[][] deck = new String[DECK_SIZE][2];
+        for (int i = 0; i < DECK_SIZE; i++) {
+            deck[i][0] = RANKS[ i % 13 ];
+            deck[i][1] = SUITS[ (i / 13) % 4 ];
+        }
+        return deck;
+    }
+
+    public static void printCardDeck(String[][] deck) {
+        for (int i = 0; i < deck.length; i++ ) {
+            if ( ( i + 1 ) % 13 == 0) {
+                printAry(deck[i]);
+                System.out.println();
+            } else {
+                printAry(deck[i]);
+                System.out.print("  ");
+            }
+        }
+    }
 }
